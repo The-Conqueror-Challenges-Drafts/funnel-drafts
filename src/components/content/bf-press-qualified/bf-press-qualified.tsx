6b9adb1973c2561ae2cec78f1ed8de7d49a512d1
alex-funnel <alex.piele@theconqueror.com>
2025-11-14 11:41:17 +0200

    Fix useRef import

--- src/components/content/bf-press-qualified/bf-press-qualified.tsx
+++ src/components/content/bf-press-qualified/bf-press-qualified.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { Button } from "@/components/ui/button"
 
 interface MedalImage {
